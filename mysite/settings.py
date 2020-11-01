d4e75211f1b01c0b1058735683f05be342d84d0a
doiko123 <ayanottyz@gmail.com>
2020-11-01 16:15:45 +0900

    settings.pyの秘密鍵等をdjango-environの環境変数へ変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,17 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-try:
-    from local_settings import *
-except ImportError:
-    pass
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -79,18 +81,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'url_shortener_db',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': 'doiko',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': ''
-    }
+    'default': env.db(),
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
